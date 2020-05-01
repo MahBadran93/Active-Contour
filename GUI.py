a0547318461878502fee79895c9aa104acb60845
MahBadran93 <mahmoud.s.badran@gmail.com>
2020-05-01 12:44:47 +0200

    edit some functions and add segmented image to label

--- GUI.py
+++ GUI.py
@@ -25,6 +25,7 @@ from PyQt5.QtWidgets import QMessageBox, QDialogButtonBox
 from numpy.lib import recfunctions as rfn
 import  pandas as pd
 from skimage.draw import polygon, line, set_color
+import  PIL.Image as imgCnv
 
 
 
@@ -53,15 +54,12 @@ class Ui_Dialog(object):
         self.Upload_bott.setGeometry(QtCore.QRect(120, 15, 101, 23))
         self.Upload_bott.setObjectName("Upload_bott")
         self.Segment_bott = QtWidgets.QPushButton(Dialog)
-        self.Segment_bott.setGeometry(QtCore.QRect(350, 380, 80, 23))
+        self.Segment_bott.setGeometry(QtCore.QRect(395, 180, 80, 23))
         self.Segment_bott.setObjectName("Segment_bott")
         self.label_2 = QtWidgets.QLabel(Dialog)
         self.label_2.setGeometry(QtCore.QRect(500, 20, 151, 16))
         self.label_2.setObjectName("label_2")
-        self.image = QtWidgets.QLabel(Dialog)
-        self.image.setGeometry(QtCore.QRect(320, 50, 271, 211))
-        self.image.setText("")
-        self.image.setObjectName("image")
+      
         self.imageTest = QtWidgets.QLabel(Dialog)
         self.imageTest.setStyleSheet("background-color: white; border: 1px inset grey; min-height: 200px;")
         self.imageTest.setGeometry(QtCore.QRect(5, 40, 384, 308))
@@ -131,12 +129,18 @@ class Ui_Dialog(object):
         self.gamm_label = QtWidgets.QLabel(Dialog)
         self.gamm_label.setGeometry(QtCore.QRect(500, 460, 80, 23))
 
-       
+        pal = QtGui.QPalette()
+        pal.setColor(QtGui.QPalette.Background,QtGui.QColor(174, 173, 172));
+        Dialog.autoFillBackground()
+        Dialog.setPalette(pal)
+        
+        self.Segment_bott.setEnabled(False)
+        self.cnvJPG.setEnabled(False)
       
         
-     #   self.labelLeft.move(80, 40)
-      #  self.labelRight.move(80, 80)
-       # self.labelCenter.move(80, 120)    
+        #self.labelLeft.move(80, 40)
+        #self.labelRight.move(80, 80)
+        #self.labelCenter.move(80, 120)    
 
     
         
@@ -146,7 +150,6 @@ class Ui_Dialog(object):
 
 
         
-        self.count = 0
         self.msg = QtWidgets.QMessageBox()
         self.last_x, self.last_y = None, None
         self.apt = 0
@@ -158,6 +161,7 @@ class Ui_Dialog(object):
         
         self.ss1=0
         self.imginit=0
+        self.flagLabelnotempty = 0
         
         self.retranslateUi(Dialog)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
@@ -204,24 +208,33 @@ class Ui_Dialog(object):
 
 
 
-        
+    # this function is used  to load dicom data   
     def loadDicom(self):
+        # clear the label each time we load an image 
+        self.segmetLabel.clear()
+        self.imageTest.clear()
+        # empty the list of coords each load of new DICOM image
+        self.listOfCoords1.clear()
+        self.listOfCoords2.clear()
+        self.listOfCoords3.clear()
+        
+        # Flags to not draw more that 3 contours 
+        self.countCoords = 0
+        
+        self.flagLabelnotempty = 1
         
         # open browse dialog 
         fileName = QFileDialog.getOpenFileName()
         
-        #empty the list of coords each load of new DICOM image
-        self.countCoords = 0
+       
+
         
-        self.listOfCoords1.clear()
-        self.listOfCoords2.clear()
-        self.listOfCoords3.clear()
+        
         
         # load dicom file
         self.dataset = pydicom.dcmread(fileName[0])
         # normalize the image to darken the dicom slice image
         self.imginit = self.dataset.pixel_array
-        print('shape',self.imginit.shape, type(self.imginit) , self.imginit.dtype)
         self.threshold = 500 # Adjust as needed
         self.image_2d_scaled = (np.maximum(self.imginit, 0) / (np.amax(self.imginit) + self.threshold)) * 255.0 
         self.img=qimage2ndarray.array2qimage(self.image_2d_scaled)
@@ -240,92 +253,181 @@ class Ui_Dialog(object):
         self.pixmap = QtGui.QPixmap(self.img)    
         self.imageTest.setPixmap(self.pixmap.scaled(self.imageTest.width(),self.imageTest.height()))
         self.imageTest.mouseMoveEvent = self.drawMove
-        
-        
-
-        
-        
-
-    
+            
+    # Activates when the user start press and moving the mouse to draw the contours
     def drawMove(self, e):
+        if(self.countCoords > 2 ):
+            self.msg.setWindowTitle("Warning")
+            self.msg.setInformativeText('3 segmented objects needed!')
+            self.msg.exec()
+        else:     
         
-        if self.last_x is None: # First event.
+            if self.last_x is None: # First event.
+                self.last_x = e.x()
+                self.last_y = e.y()
+                return # Ignore the first time.
+            
+            # Pen drawing settings 
+            painter = QtGui.QPainter(self.imageTest.pixmap())
+            p = painter.pen()
+            p.setWidth(1)
+            p.setColor(QtGui.QColor('#FF0000'))
+            painter.setPen(p)
+            painter.drawLine(self.last_x, self.last_y, e.x(), e.y())
+            painter.end()
+           
+            # Update the origin for next time.
             self.last_x = e.x()
             self.last_y = e.y()
-            return # Ignore the first time.
-        
-        # Pen drawing settings 
-        painter = QtGui.QPainter(self.imageTest.pixmap())
-        p = painter.pen()
-        p.setWidth(4)
-        p.setColor(QtGui.QColor('#FF0000'))
-        painter.setPen(p)
-        painter.drawLine(self.last_x, self.last_y, e.x(), e.y())
-        painter.end()
-       
-        # Update the origin for next time.
-        self.last_x = e.x()
-        self.last_y = e.y()
-        print(self.last_x, self.last_y)
-        
-        # Save the manual segmented coordinates to list 
-        if(self.countCoords == 0):
-            self.listOfCoords1.append([self.last_x,self.last_y])
+            #print(self.last_x, self.last_y)
             
-        if(self.countCoords == 1):
-            self.listOfCoords2.append([self.last_x,self.last_y])
+            # Save the manual segmented coordinates to list 
+            if(self.countCoords == 0):
+                self.listOfCoords1.append([self.last_x,self.last_y])
+                
+            if(self.countCoords == 1):
+                self.listOfCoords2.append([self.last_x,self.last_y])
+                
+            if(self.countCoords == 2):
+                self.listOfCoords3.append([self.last_x,self.last_y])    
             
-        if(self.countCoords == 2):
-            self.listOfCoords3.append([self.last_x,self.last_y])    
-        
-        # release mouse 
-        self.imageTest.mouseReleaseEvent = self.release
+            # release mouse 
+            self.imageTest.mouseReleaseEvent = self.release
+         
+            # not drawing outside the image 
+            if(self.last_x > self.imageTest.width() or self.last_y > self.imageTest.height() or self.last_y < 0 or self.last_x < 0 ):
+                self.msg.setWindowTitle("Image")
+                self.msg.setInformativeText('Out of image label.')
+                self.msg.exec()
+                self.last_x = None
+                self.last_y = None
+                
+            # Refresh    
+            self.imageTest.update()
      
-        # not drawing outside the image 
-        if(self.last_x > self.imageTest.width() or self.last_y > self.imageTest.height() or self.last_y < 0 or self.last_x < 0 ):
-            self.msg.setWindowTitle("Image")
-            self.msg.setInformativeText('Out of image label.')
-            self.msg.exec()
-            self.last_x = None
-            self.last_y = None
-            
-        # Refresh    
-        self.imageTest.update()
-     
-        
+    # Mouse event, when the user release the mouse this function activates     
     def release(self, e):
         self.last_x = None
         self.last_y = None
         self.countCoords += 1
-        
-    def savePNG(self):
-        if(self.countCoords != 3 ):
-            self.msg.setWindowTitle("Warning")
-            self.msg.setInformativeText('3 segmented objects needed!')
-            self.msg.exec()
-        else:
-            # pop up message to check image is saved
-            self.msg.setWindowTitle("Save To JPG ")
-            self.msg.setInformativeText('Image has been Saved.')
-            self.msg.exec()
-            self.path = './Dicom_Slices'
-            # Save Dicom MRI image to file 
-            cv2.imwrite(os.path.join(self.path,'testImage{0}.jpg'.format(self.count)), self.image_2d_scaled) 
-            
-               
-            # normalize the labeled image
-            self.qImg = self.pixmap.toImage()
-            
-            #s = self.qImg.bits().asstring((self.imageTest.width * self.imageTest.height) * channels_count)
-            #arr = np.fromstring(s, dtype=np.uint8).reshape((self.imageTest.height, self.imageTest.width, channels_count))
-            self.labeledImg = qimage2ndarray.recarray_view(self.qImg)
-            self.threshold = 500 # Adjust as needed
-            #self.imgLabeledScaled = (np.maximum(self.labeledImg, 0) / (np.amax(self.labeledImg) + self.threshold)) * 255.0 
-            #print('shape2' , self.labeledImg.shape , type(self.labeledImg))
-            #cv2.imwrite(os.path.join(self.path,'testImageLabeled{0}.jpg'.format(self.count)), np.array(self.qImg)) 
+        if (self.countCoords == 3):
+            # list of coords to numpy array 
             self.ss1 = np.array(self.listOfCoords1)
             self.ss2 = np.array(self.listOfCoords2)
             self.ss3 = np.array(self.listOfCoords3)
+            self.Segment_bott.setEnabled(True)
+        
+    # this function activates when the user press JPG button, which saves the two images     
+    def savePNG(self):     
+        self.path = './Dicom_Slices'
+        # Save Dicom MRI image to file 
+        cv2.imwrite(os.path.join(self.path,'testImage{0}.jpg'.format(self.dataset.InstanceNumber)), self.image_2d_scaled) 
+        cv2.imwrite(os.path.join(self.path,'segmentedImg{0}.jpg'.format(self.dataset.InstanceNumber)), self.img) 
+        # pop up message to check image is saved
+        self.msg.setWindowTitle("Save To JPG ")
+        self.msg.setInformativeText('Images has been Saved.')
+        self.msg.exec()
+               
+           
+    # this function used to plot the labeled image, not used in the gui currently  
+    def plotLabeledImg(self):
+        # normalize the labeled image
+        self.qImg = self.pixmap.toImage()
+        
+        self.labeledImg = qimage2ndarray.recarray_view(self.qImg)
+        self.threshold = 500 # Adjust as needed
+        
+
+        
+        plt.plot(self.ss1[:,0],self.ss1[:,1],'r')
+        plt.plot(self.ss2[:,0],self.ss2[:,1],'r')
+
+        plt.plot(self.ss3[:,0],self.ss3[:,1],'r')
+
+        self.img = np.zeros((308, 384), dtype=np.uint8)
+        set_color(self.imginit, (self.ss1[:,1],self.ss1[:,0]), 1)
+        plt.imshow(self.imginit)
+        plt.show()
+     
+    # Activate when the user press 'segment' button    
+    def active_contour(self):
+        
+        # enable saving the image button 
+        self.cnvJPG.setEnabled(True)
+        
+        # clear the label each time the user press segment, to not store alot above each other 
+        self.segmetLabel.clear()
+  
+        
+        # convert to gray scale image 
+        self.img = rgb2gray(self.image_2d_scaled.copy())
+        
+        #  values of alpha, beta, gamma, that active contour function requires 
+        alpha=self.alpha.value()
+        beta=self.beta.value()
+        gamma=self.gama.value()
+        
+        # Convert coordinates to numpy array
+        init1 = np.array([self.ss1[:,0],self.ss1[:,1]]).T
+        init2 = np.array([self.ss2[:,0],self.ss2[:,1]]).T
+        init3 = np.array([self.ss3[:,0],self.ss3[:,1]]).T
+
+        # Apply the Active Contour functions on our 3 contours 
+        snake1 = active_contour(gaussian(self.img, 3),
+                               init1, alpha, beta,gamma,max_iterations=2500,  
+                               coordinates='rc')
+        
+        
+        snake2 = active_contour(gaussian(self.img, 3),
+                                init2, alpha, beta,gamma,max_iterations=2500,  
+                                coordinates='rc')
+        snake3 = active_contour(gaussian(self.img, 3),
+                                init3, alpha, beta,gamma,max_iterations=2500,  
+                                coordinates='rc')
+        
+        '''
+        fig, ax = plt.subplots(figsize=(7, 7))
+        ax.imshow(img, cmap=plt.cm.gray)
+        ax.plot(init1[:, 0], init1[:, 1], '-r', lw=3)
+        ax.plot(snake1[:,,0 0], snake1[:, 1], '-b', lw=3)
+        ax.plot(init2[:, 0], init2[:, 1], '-r', lw=3)
+        ax.plot(snake2[:, 0], snake2[:, 1], '-b', lw=3)
+        ax.plot(init3[:, 0], init3[:, 1], '-r', lw=3)
+        ax.plot(snake3[:, 0], snake3[:, 1], '-b', lw=3)
+        ax.set_xticks([]), ax.set_yticks([])
+        ax.axis([0, img.shape[1], img.shape[0], 0])
+        '''
+        
+        #Segment the snakes and edit on the image 
+        cv2.fillConvexPoly(self.img,np.array(snake1,'int32'),(180, 0, 0))
+        cv2.fillConvexPoly(self.img,np.array(snake2,'int32'),(100, 0, 50))
+        cv2.fillConvexPoly(self.img,np.array(snake3,'int32'),(250, 15, 0))
+
+        
+        self.segmentedImg = qimage2ndarray.array2qimage(self.img)
+        self.pixmap2 = QtGui.QPixmap(self.segmentedImg)    
+        self.segmetLabel.setPixmap(self.pixmap2.scaled(self.imageTest.width(),self.imageTest.height()))
+        
+    
+    
+    
+                        
+                
+
+        
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Dialog = QtWidgets.QDialog()
+    ui = Ui_Dialog()
+    ui.setupUi(Dialog)
+    Dialog.show()
+    Dialog.update()
+    sys.exit(app.exec_())
+    
+    
+    
+    '''
             #plt.imshow(self.imginit)
             
             #plt.plot(ss1[:,0],ss1[:,1],'r')
@@ -341,84 +443,5 @@ class Ui_Dialog(object):
             #poly3 = Polygon([p[0],p[1]] for p in self.ss3)
             #self.x3,self.y3 = poly3.convex_hull.exterior.coords.xy
             
-            
-            plt.plot(self.ss1[:,0],self.ss1[:,1],'r')
-            plt.plot(self.ss2[:,0],self.ss2[:,1],'r')
-
-            plt.plot(self.ss3[:,0],self.ss3[:,1],'r')
-
-            self.img = np.zeros((308, 384), dtype=np.uint8)
-            set_color(self.imginit, (self.ss1[:,1],self.ss1[:,0]), 1)
-            plt.imshow(self.imginit)
-            plt.show()
-            
-            
     
-         
-            
-            
-    def active_contour(self):
-        img = rgb2gray(self.image_2d_scaled)
-        alpha=self.alpha.value()
-        beta=self.beta.value()
-        gamma=self.gama.value()
-
-        
-        init1 = np.array([self.ss1[:,0],self.ss1[:,1]]).T
-        init2 = np.array([self.ss2[:,0],self.ss2[:,1]]).T
-
-        init3 = np.array([self.ss3[:,0],self.ss3[:,1]]).T
-        
-    
-        '''  ..
-        (image, snake, alpha=0.01, beta=0.1,
-                   w_line=0, w_edge=1, gamma=0.01,
-                   bc=None, max_px_move=1.0,
-                   max_iterations=2500, convergence=0.1,
-                   *,
-                   boundary_condition='periodic',
-                   coordinates=None):
-            
-        '''
-     
-        snake1 = active_contour(gaussian(img, 1),
-                               init1, alpha, beta,gamma,max_iterations=5000,  
-                               coordinates='rc')
-        
-        
-        snake2 = active_contour(gaussian(img, 3),
-                                init2, alpha, beta,gamma,max_iterations=5000,  
-                                coordinates='rc')
-        snake3 = active_contour(gaussian(img, 3),
-                                init3, alpha, beta,gamma,max_iterations=5000,  
-                                coordinates='rc')
-        
-        
-        fig, ax = plt.subplots(figsize=(7, 7))
-        ax.imshow(img, cmap=plt.cm.gray)
-        ax.plot(init1[:, 0], init1[:, 1], '-r', lw=3)
-        ax.plot(snake1[:, 0], snake1[:, 1], '-b', lw=3)
-        ax.plot(init2[:, 0], init2[:, 1], '-r', lw=3)
-        ax.plot(snake2[:, 0], snake2[:, 1], '-b', lw=3)
-        ax.plot(init3[:, 0], init3[:, 1], '-r', lw=3)
-        ax.plot(snake3[:, 0], snake3[:, 1], '-b', lw=3)
-        ax.set_xticks([]), ax.set_yticks([])
-        ax.axis([0, img.shape[1], img.shape[0], 0])
-        
-        plt.show()
-        
-        
-        
-                            
-                
-
-        
-if __name__ == "__main__":
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
-    Dialog = QtWidgets.QDialog()
-    ui = Ui_Dialog()
-    ui.setupUi(Dialog)
-    Dialog.show()
-    Dialog.update()
-    sys.exit(app.exec_())
+    '''
